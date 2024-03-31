74c0cd69177e925021af3ec75eeeea5bcd72702b
Mohid Mohan <mohidmohan8482@gmail.com>
2024-03-31 09:51:15 +0530

    first _commit

---
+++ src/App.js
@@ -0,0 +1,25 @@
+import './Components/NavBar/NavBar.css'
+import NavBar from './Components/NavBar/NavBar';
+import './App.css'
+import Banner from './Components/Banner/Banner';
+import RowPost from './Components/RowPost/RowPost';
+import axios from "axios"
+
+
+function App() {
+  return (
+    <>
+    <div className='App'>
+      <NavBar/>
+      <Banner/>
+      <RowPost/>
+
+      <button  onClick={() =>{
+        axios.get()
+      } }>Click me</button>
+    </div>
+    </>
+  );
+}
+
+export default App;
